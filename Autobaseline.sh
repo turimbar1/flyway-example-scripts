a624f3d178b8fb50c9fb91f18df27372320f815f
Andrew Pierce <andrew.craig.pierce@gmail.com>
2024-10-29 08:42:37 -0700

    dev updated code for mastercard ticket 310150

--- Autobaseline.sh
+++ Autobaseline.sh
@@ -1,49 +1,155 @@
-#!/bin/bash
-set -euo pipefail
+#!/bin/bash 
 
-# Point to where flyway dev is
-# If you didn't create the entrypoint script you may need to invoke flyway dev through dotnet
-# e.g. /opt/flyway-desktop/dotnet/dotnet /opt/flyway-desktop/flyway-dev/flyway-dev.dll
-flyway-dev() {
-    /opt/flyway-desktop/flyway-dev.sh --i-agree-to-the-eula "$@"
-}
+  
 
-# Set the working folder path
-WorkingFolderPath=~/.
+########### To generate new baseline and schema-model, update WorkingFolderPath to a new location may be something with app name,Url,Password, Schemas 
 
-# Set the database type and database connection properties
-DatabaseType="Oracle" # alt values: SqlServer Oracle PostgreSql 
-Url="jdbc:oracle:thin:@//localhost:1521/Dev1"
-User="HR"
-Password="Password"
-Schemas='"HR"' # May be '' for SqlServer or '"Schema1", "Schema2"' for Oracle
+  
 
-# Set the paths
-ArtifactPath="/tmp/artifact.zip"
-ProjectPath="$WorkingFolderPath/flyway.toml"
-MigrationPath="$WorkingFolderPath/migrations"
+set -euo pipefail 
 
-# Create a project
-flyway-dev init -n Autobaseline -p "$WorkingFolderPath" --database-type "$DatabaseType"
+  
 
-echo -e "\n\n[environments.development]\nurl = \"some-url\"\nschemas = [$Schemas]" >> "$ProjectPath"
+  
 
-# schema model diffs
-DiffOptions=$(cat <<-END
-{ "url": "$Url", "user": "$User", "password": "$Password", "schemas": [$Schemas], "resolverProperties": [] } 
-END
-)
+# Set the working folder path 
 
-echo "$DiffOptions" \
-  | flyway-dev diff -p "$ProjectPath" -a "$ArtifactPath" --from Target --to SchemaModel
+export DOTNET_ROOT=/opt/ddl_automation/OEL8/flyway-desktop-7.6.2/dotnet 
 
-#apply to schema model
-flyway-dev take -p "$ProjectPath" -a "$ArtifactPath" \
-  | flyway-dev apply -p "$ProjectPath" -a "$ArtifactPath"
+export PATH=${PATH}:/etc:/usr/lib:/usr/lib64:/usr/local/bin:/sys_bckup/reel/bin:/usr/etc:/usr/bin/X11:/etc/conf/bin:/oracle/local/bin:/opt/ddl_automation/OEL8/flyway-10.17.1:/opt/ddl_automation/instantclient_19_15:/opt/ddl_automation/OEL8/flyway-desktop-7.6.2/flyway-dev:/opt/ddl_automation/OEL8/flyway-desktop-7.6.2/dotnet 
 
-echo "$DiffOptions" \
-  | flyway-dev diff -p "$ProjectPath" -a "$ArtifactPath" --from Target --to Empty
+  
 
-# Generate the baseline from all differences
-flyway-dev take -p "$ProjectPath" -a "$ArtifactPath" \
-  | flyway-dev generate -p "$ProjectPath" -a "$ArtifactPath" -o "$MigrationPath" --name 'B1__baseline.sql' --versioned-only
+export PROGRAM=$1 
+
+echo $PROGRAM 
+
+  
+
+export CRQ=$2 
+
+echo $CRQ 
+
+  
+
+  
+
+# Set the working folder path 
+
+WorkingFolderPath="/ddl_automation/Automated_Onboarding/"$PROGRAM"/$CRQ/Baselinemodel" 
+
+  
+
+export DBTYPE=$3 
+
+echo $DBTYPE 
+
+  
+
+export DBBASELINEURL=$4 
+
+echo $DBBASELINEURL 
+
+  
+
+export DBUSER=$5 
+
+echo $DBUSER 
+
+  
+
+export DBPWD=$6 
+
+echo $DBPWD 
+
+  
+
+export SCHEMAS=$7 
+
+echo $SCHEMAS 
+
+  
+
+# Set the database type and database connection properties 
+
+DatabaseType="$DBTYPE" # alt values: SqlServer Oracle PostgreSql 
+
+#Url="jdbc:oracle:thin:@//x0stl11-scan.mastercard.int:1527/PDBCDPCS01S" 
+
+Url="$DBBASELINEURL" 
+
+User="$DBUSER" 
+
+Password="$DBPWD" 
+
+Schemas="$SCHEMAS" # May be '' for SqlServer or '"Schema1", "Schema2"' for Oracle 
+
+echo $SCHEMAS 
+
+  
+
+# Set the paths 
+
+ArtifactPath="$WorkingFolderPath/artifact.zip" 
+
+ProjectPath="$WorkingFolderPath/flyway.toml" 
+
+MigrationPath="$WorkingFolderPath/migrations" 
+
+  
+
+# Create a project 
+
+flyway-dev init -n Autobaseline -p "$WorkingFolderPath" --database-type "$DatabaseType" --i-agree-to-the-eula 
+
+echo -e "\n\n[environments.development]\nurl = \"some-url\"\nschemas = [$Schemas]"" >> "$ProjectPath" 
+
+echo -e "\n\n[environments.target]\nurl = "$Url"\nschemas = [$Schemas]\nuser = "$User"\npassword = "$Password"" >> "$ProjectPath" 
+
+sed -i 's/includeStoragePartitioning = false/includeStoragePartitioning = true/g' "$ProjectPath" 
+
+sed -i 's/ignoreSupplementalLogGroups = false/ignoreSupplementalLogGroups = true/g' "$ProjectPath" 
+
+sed -i 's/ignorePermissions = true/ignorePermissions = false/g' "$ProjectPath" 
+
+  
+
+# schema model diffs 
+
+###DiffOptions=$(cat <<-END 
+
+###{ "url": "$Url", "user": "$User", "password": "$Password", "schemas": [$Schemas], "resolverProperties": [] } 
+
+###END 
+
+###) 
+
+  
+
+  
+
+flyway-dev diff -p "$ProjectPath" -a "$ArtifactPath" --targetId target --from Target --to SchemaModel --i-agree-to-the-eula 
+
+  
+
+#apply to schema model 
+
+flyway-dev take -p "$ProjectPath" -a "$ArtifactPath" --i-agree-to-the-eula \ 
+
+  | flyway-dev apply -p "$ProjectPath" -a "$ArtifactPath" --i-agree-to-the-eula 
+
+  
+
+flyway-dev diff -p "$ProjectPath" -a "$ArtifactPath" --targetId target --from Target --to Empty --i-agree-to-the-eula 
+
+  
+
+# Generate the baseline from all differences 
+
+flyway-dev take -p "$ProjectPath" -a "$ArtifactPath" --i-agree-to-the-eula \ 
+
+  | flyway-dev generate -p "$ProjectPath" -a "$ArtifactPath" -o "$MigrationPath" --name 'B1__script.sql' --versioned-only --i-agree-to-the-eula 
+
+  
+
+echo -e "\n[flyway.oracle]\nsqlplus = true" >> "$ProjectPath"
