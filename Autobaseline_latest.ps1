71a9c567cad1070e1ef6a47e1c3628c29e1d5e2f
Andrew Pierce <andrew.craig.pierce@gmail.com>
2025-02-07 17:12:14 -0800

    variables

--- Autobaseline_latest.ps1
+++ Autobaseline_latest.ps1
@@ -1,5 +1,15 @@
-mkdir Autobaseline
-cd ./Autobaseline
-flyway init "-init.projectName=Autobaseline" "-init.databaseType=SqlServer"
-flyway diff model "-diff.source=dev" "-diff.target=schemaModel" "-environments.dev.url=jdbc:sqlserver://localhost;databaseName=NewWorldDB_Dev;encrypt=false;integratedSecurity=true;trustServerCertificate=true"
+# Set the database type and database connection properties
+$databaseType = "SqlServer" # alt values: SqlServer Oracle PostgreSql MySql 
+$Url = "jdbc:sqlserver://localhost;databaseName=NewWorldDB_Dev;encrypt=false;integratedSecurity=true;trustServerCertificate=true"
+$User = ""
+$Password = ""
+$projectName = "Autobaseline"
+
+# Set the schemas value
+$schemas = @("") # can be empty for SqlServer
+
+mkdir $projectName
+cd ./$projectName
+flyway init "-init.projectName=$projectName" "-init.databaseType=$databaseType"
+flyway diff model "-diff.source=dev" "-diff.target=schemaModel" "-environments.dev.url=$Url" "-environments.dev.user=$User" "-environments.dev.password=$Password" #environments.dev.schemas
 flyway diff generate "-diff.source=schemaModel" "-diff.target=empty" "-generate.types=baseline" "-generate.version=1.0.0" 
