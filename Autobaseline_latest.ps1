06f4f94a1361d4170f328c3993d4475935c59d6a
turimbar1 <Andrew.pierce@red-gate.com>
2025-03-04 09:31:53 -0800

    Update Autobaseline_latest.ps1
    added schemas

--- Autobaseline_latest.ps1
+++ Autobaseline_latest.ps1
@@ -11,5 +11,5 @@ $schemas = @("") # can be empty for SqlServer
 mkdir $projectName
 cd ./$projectName
 flyway init "-init.projectName=$projectName" "-init.databaseType=$databaseType"
-flyway diff model "-diff.source=dev" "-diff.target=schemaModel" "-environments.dev.url=$Url" "-environments.dev.user=$User" "-environments.dev.password=$Password" #environments.dev.schemas
+flyway diff model "-diff.source=dev" "-diff.target=schemaModel" "-environments.dev.url=$Url" "-environments.dev.user=$User" "-environments.dev.password=$Password" "-environments.dev.schemas=$schemas"
 flyway diff generate "-diff.source=schemaModel" "-diff.target=empty" "-generate.types=baseline" "-generate.version=1.0.0" 
